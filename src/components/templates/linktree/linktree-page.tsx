0cc5c1b812a55828b9eee68eb75d67faa69f1896
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-17 19:40:20 -0300

    Add clsx and tailwind-merge dependencies; refactor LinktreePage component
    - Added clsx and tailwind-merge to package.json and package-lock.json.
    - Refactored LinktreePage component to utilize utility classes for styling.
    - Introduced a new utility function in utils.ts for class name merging.

--- src/components/templates/linktree/linktree-page.tsx
+++ src/components/templates/linktree/linktree-page.tsx
@@ -1,3 +1,15 @@
+import { cn } from '@/src/lib/utils';
+
 export function LinktreePage() {
-  return <div>LinktreePage</div>;
+  return (
+    <main
+      className={cn(
+        'flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat',
+        'bg-[url("/bg-mobile-light.jpg")] dark:bg-[url("/bg-mobile.jpg")]',
+        'lg:bg-[url("/bg-desktop-light.jpg")] dark:lg:bg-[url("/bg-desktop.jpg")]',
+      )}
+    >
+      LinktreePage
+    </main>
+  );
 }
