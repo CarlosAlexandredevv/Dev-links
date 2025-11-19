1583e61dcc370f2cb2ac57ac9501e4bd04b37eed
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-19 19:14:45 -0300

    Enhance button styling in SocialLink component
    - Updated button component in SocialLink to include a duration for hover transitions, improving user experience and visual feedback.

--- src/components/social-link/social-link.tsx
+++ src/components/social-link/social-link.tsx
@@ -6,7 +6,7 @@ interface SocialLinkProps {
 }
 export function SocialLink({ href, icon }: SocialLinkProps) {
   return (
-    <button className="w-14 h-14 flex items-center justify-center rounded-full hover:bg-highlight transition-colors cursor-pointer">
+    <button className="w-14 h-14 flex items-center justify-center rounded-full hover:bg-highlight transition-colors duration-300 cursor-pointer">
       <a
         className="text-2xl cursor-pointer text-black dark:text-white flex items-center justify-center"
         href={href}
