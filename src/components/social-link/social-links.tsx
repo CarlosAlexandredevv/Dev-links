69b0c653e51478a80e0062aa622a919c02743411
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-19 19:19:09 -0300

    Swap Instagram and YouTube icons in social links component for accurate representation

--- src/components/social-link/social-links.tsx
+++ src/components/social-link/social-links.tsx
@@ -5,11 +5,11 @@ export const socialLinks = [
   },
   {
     href: 'https://www.linkedin.com/in/carlos-alexandre-375a9227b/',
-    icon: <ion-icon name="logo-youtube"></ion-icon>,
+    icon: <ion-icon name="logo-instagram"></ion-icon>,
   },
   {
     href: 'https://www.linkedin.com/in/carlos-alexandre-375a9227b/',
-    icon: <ion-icon name="logo-instagram"></ion-icon>,
+    icon: <ion-icon name="logo-youtube"></ion-icon>,
   },
   {
     href: 'https://www.linkedin.com/in/carlos-alexandre-375a9227b/',
